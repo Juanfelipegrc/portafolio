5dc54c446b2f9f72ecc2bd420348ce3b4867c58b
Juan Felipe García Rojas <felipegarciarjs@gmail.com>
2025-03-07 18:59:31 -0500

    Fifth Commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as motion from 'motion/react-client'
 import { useMotionValue, useSpring} from 'motion/react'
 import { FloatCube, FloatCubeNormal } from '../components';
@@ -13,7 +13,7 @@ export const HomePage = () => {
     const smoothX = useSpring(mouseX, { stiffness: 300, damping: 28 });
     const smoothY = useSpring(mouseY, { stiffness: 300, damping: 28 });
 
-
+    const [cursorSize, setCursorSize] = useState(1);
 
     useEffect(() => {
         const updateCursor = (e) => {
@@ -27,7 +27,13 @@ export const HomePage = () => {
 
 
 
-    
+    const handleClick = () => {
+        setCursorSize(1.7); 
+
+        setTimeout(() => {
+            setCursorSize(1); 
+        }, 200);
+    };
     
 
 
@@ -39,6 +45,7 @@ export const HomePage = () => {
     
         <div 
             className='flex flex-col w-full min-h-screen items-center justify-center relative overflow-hidden bg-black'
+            onMouseDown={handleClick}
         >
 
             <div 
@@ -54,14 +61,21 @@ export const HomePage = () => {
             </div>
 
             <motion.div
-                className="hidden sm:hidden md:hidden lg:inline-block fixed top-0 left-0 w-6 h-6 bg-neutral-700 rounded-full pointer-events-none mix-blend-difference"
+                className="hidden sm:hidden md:hidden lg:flex fixed top-0 left-0 w-6 h-6 bg-transparent border-2 border-white rounded-full pointer-events-none mix-blend-difference justify-center items-center"
                 style={{
                     x: smoothX,
                     y: smoothY,
-                    transform: "translate(-20%, -20%)",
+                    transition: 'scale 0.2s ease-out',
+                    transform: "translate(-50%, -50%)",
                     zIndex: 999,
                 }}
-            />
+                animate={{scale: cursorSize}}
+                transition={{duration: 0.2, ease: 'easeOut'}}
+            >
+                <div className='text-white text-[1.8rem] mb-[1.2rem] flex justify-center items-center'>
+                    .
+                </div>
+            </motion.div>
 
             
 
@@ -69,11 +83,21 @@ export const HomePage = () => {
                 <FloatCubeNormal/>
             
                     <h1 
-                        style={{fontFamily: 'Turret Road'}}
-                        className='text-3xl md:text-6xl lg:text-7xl text-center text-white z-[2]'
+                        style={{fontFamily: 'Rubik Iso'}}
+                        className='text-[1.7rem] md:text-6xl lg:text-7xl text-center text-white z-[2]'
                     >
-                        HI DEAR, I'M REACT JS SOFTWARE DEVELOPER
+                        HI DEAR, I'M JUAN FELIPE <br/>
+                        WHAT CAN I DO FOR YOU?
                     </h1>
+
+                    <div className='flex justify-center items-center gap-10 mt-5'>
+                        <button style={{fontFamily: 'Turret Road'}} className='bg-transparent text-white text-[1rem] lg:text-[1.2rem] font-medium'>
+                            See projects
+                        </button>
+                        <button style={{fontFamily: 'Turret Road'}} className='bg-transparent text-white text-[1rem] lg:text-[1.2rem] font-medium'>
+                            See projects
+                        </button>
+                    </div>
 
         
                 <FloatCube/>
