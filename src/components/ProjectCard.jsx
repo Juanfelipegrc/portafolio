8979afb3448e4b09adc0c764854fa6acd3ca59c6
Juan Felipe García Rojas <felipegarciarjs@gmail.com>
2025-03-15 17:25:59 -0500

    Sixteenth Commit

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -52,59 +52,61 @@ export const ProjectCard = ({image, title, tecnologies, desc}) => {
   return (
     <>
     
-        <motion.div
-                layout
-                transition={{
-                    layout: { duration: 0.2, ease: 'easeInOut' }
-                  }}
-                className={`bg-neutral-900 col-span-12 md:col-span-4 lg:col-span-4 flex flex-col gap-3 ${cardClicked.validation && cardClicked.card === title && !isClosing? 'fixed top-0 left-0 w-screen h-screen z-[1000] rounded-none p-10' : isClosing? 'bottom-4 w-auto h-auto' : 'rounded-4xl p-6'} `}
-            >
-            <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                height="1.5rem" 
-                viewBox="0 -960 960 960" 
-                width="1.5rem" 
-                className={`${cardClicked.validation && !isClosing? 'mt-8' : 'hidden'}`}
-                onClick={onClose}
-                fill="#fff">
-                    <path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/>
-            </svg>
-            <img 
-                src={image} 
-                alt="Austronaut logo" 
-                className='w-16' 
-            />
-            <h2 className='text-white text-xl font-semibold'>{title}</h2>
-            <p className='text-white text-sm'>{desc.length > 300 && !cardClicked.validation? `${descCut}...` : desc}</p>
-            <div className='flex items-center gap-2'>
-                {
-                    tecnologies.map((tecnology, index) => {
+        <div className='col-span-12 md:col-span-4 lg:col-span-4 h-[23rem] sm:h-[20rem] md:h-[31.5rem] lg:h-[27rem]'>
+            <motion.div
+                    layout
+                    transition={{
+                        layout: { duration: 0.2, ease: 'easeInOut' }
+                    }}
+                    className={`bg-neutral-900 flex flex-col justify-center gap-3 ${cardClicked.validation && cardClicked.card === title && !isClosing? 'fixed top-0 left-0 w-screen h-screen z-[1000] rounded-none p-10' : isClosing? 'bottom-4 w-auto h-auto' : 'rounded-4xl p-6 h-full'} `}
+                >
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="1.5rem" 
+                    viewBox="0 -960 960 960" 
+                    width="1.5rem" 
+                    className={`${cardClicked.validation && !isClosing? 'mt-8' : 'hidden'}`}
+                    onClick={onClose}
+                    fill="#fff">
+                        <path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/>
+                </svg>
+                <img 
+                    src={image} 
+                    alt="Austronaut logo" 
+                    className='w-16' 
+                />
+                <h2 className='text-white text-xl font-semibold'>{title}</h2>
+                <p className='text-white text-sm'>{desc.length > 300 && !cardClicked.validation? `${descCut}...` : desc}</p>
+                <div className='flex items-center gap-2'>
+                    {
+                        tecnologies.map((tecnology, index) => {
 
-                        const bgColor = getBgColor(tecnology);
+                            const bgColor = getBgColor(tecnology);
 
-                        return(
-                            <span
-                                key={index} 
-                                className={`py-1 px-2 rounded-xl ${bgColor} text-white text-xs `}
-                        
-                            >
-                                {tecnology}
-                            </span>
-                        )
-                        
-                    })
-                }
-            </div>
+                            return(
+                                <span
+                                    key={index} 
+                                    className={`py-1 px-2 rounded-xl ${bgColor} text-white text-xs `}
+                            
+                                >
+                                    {tecnology}
+                                </span>
+                            )
+                            
+                        })
+                    }
+                </div>
 
-            <span 
-                className={`${cardClicked.validation? 'hidden' : 'text-blue-500 p-1 hover:underline cursor-none'}`}
-                onClick={() => setCardClicked({validation: true, card: title})}
-            >
-                See more →
-            </span>
-            
+                <span 
+                    className={`${cardClicked.validation? 'hidden' : 'text-blue-500 p-1 hover:underline cursor-none'}`}
+                    onClick={() => setCardClicked({validation: true, card: title})}
+                >
+                    See more →
+                </span>
+                
 
-        </motion.div>
+            </motion.div>
+        </div>
     
     </>
   )
